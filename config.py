fbeb4df9a03a55d7429e2bfd6afc52abe3d33260
Romain Monclus <romain.monclus@gmail.com>
2014-06-20 13:29:04 +0200

    tuto part v: openid not ok

--- config.py
+++ config.py
@@ -4,6 +4,13 @@ SECRET_KEY = 'you-will-never-guess'
 OPENID_PROVIDERS = [
 	{'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'}
 	]
+
+GOOGLE_API_CLIENT_ID = 'myid.apps.googleusercontent.com'
+GOOGLE_API_CLIENT_SECRET = 'my secret code'
+GOOGLE_API_SCOPE = 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'
+GOOGLE_OAUTH2_URL = 'https://accounts.google.com/o/oauth2/'
+GOOGLE_API_URL = 'https://www.googleapis.com/oauth2/v1/'
+
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
